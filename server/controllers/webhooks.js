346987346ddee9bd0630e1d7a06b49b67894f05d
Shivaliss15 <shivalisalunkhe15@gmail.com>
2025-04-17 18:18:55 +0530

    Latest commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,7 +4,7 @@ import User from '../models/User.js'
 
 //API controller function to manage clerk user with database
 
-const clerkWebhooks = async (req,res) => {
+export const clerkWebhooks = async (req,res) => {
   
 
     //crete a Svix instance with clerk webhook secret.
@@ -89,4 +89,3 @@ const clerkWebhooks = async (req,res) => {
     }
 };
 
-export default clerkWebhooks;
